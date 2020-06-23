b0be71e394cafb44f399157962b8f19e46170bd5
Debojit <ddas1500@gmail.com>
2020-06-23 23:46:33 +0530

    pieces can move around properly according to rules. havent implemented dice or cutting other pieces or overlap yet

--- Scene2.js
+++ Scene2.js
@@ -5,6 +5,12 @@ class Scene2 extends Phaser.Scene {
 	********************************************************/
 	constructor() {
 		super("playGame");
+		this.dice_roll = 14;
+		this.turn = 0; // 0 -> Red, 1 -> Blue, 2 -> Green, 3 -> Yellow
+		this.redScore = 0;
+		this.blueScore = 0;
+		this.greenScore = 0;
+		this.yellowScore = 0;
 	}
 
 	/********************************************************
@@ -20,32 +26,32 @@ class Scene2 extends Phaser.Scene {
 	}
 
 	layout() {
-		this.ludoboard = this.add.image(0, 0, "ludoboard");
+		this.ludoboard = this.add.image(0, 0, "ludoboard").setScale(scaling_parameter);
 		this.ludoboard.setOrigin(0, 0);
-		this.dashboard = this.add.image(300, 0, "dashboard");
+		this.dashboard = this.add.image(300*scaling_parameter, 0, "dashboard").setScale(scaling_parameter);;
 		this.dashboard.setOrigin(0, 0);
 	}
 
 	startofgame() {
-		this.red1 = this.add.image(45, 45, "red");
-		this.red2 = this.add.image(80, 45, "red");
-		this.red3 = this.add.image(45, 80, "red");
-		this.red4 = this.add.image(80, 80, "red");
+		this.red1 = this.add.image(45*scaling_parameter, 45*scaling_parameter, "red").setScale(scaling_parameter);
+		this.red2 = this.add.image(80*scaling_parameter, 45*scaling_parameter, "red").setScale(scaling_parameter);
+		this.red3 = this.add.image(45*scaling_parameter, 80*scaling_parameter, "red").setScale(scaling_parameter);
+		this.red4 = this.add.image(80*scaling_parameter, 80*scaling_parameter, "red").setScale(scaling_parameter);
 
-		this.blue1 = this.add.image(45, 218, "blue");
-		this.blue2 = this.add.image(80, 218, "blue");
-		this.blue3 = this.add.image(45, 253, "blue");
-		this.blue4 = this.add.image(80, 253, "blue");
+		this.blue1 = this.add.image(45*scaling_parameter, 218*scaling_parameter, "blue").setScale(scaling_parameter);
+		this.blue2 = this.add.image(80*scaling_parameter, 218*scaling_parameter, "blue").setScale(scaling_parameter);
+		this.blue3 = this.add.image(45*scaling_parameter, 253*scaling_parameter, "blue").setScale(scaling_parameter);
+		this.blue4 = this.add.image(80*scaling_parameter, 253*scaling_parameter, "blue").setScale(scaling_parameter);
 
-		this.green1 = this.add.image(218, 45, "green");
-		this.green2 = this.add.image(218, 80, "green");
-		this.green3 = this.add.image(253, 45, "green");
-		this.green4 = this.add.image(253, 80, "green");
+		this.green1 = this.add.image(218*scaling_parameter, 45*scaling_parameter, "green").setScale(scaling_parameter);
+		this.green2 = this.add.image(218*scaling_parameter, 80*scaling_parameter, "green").setScale(scaling_parameter);
+		this.green3 = this.add.image(253*scaling_parameter, 45*scaling_parameter, "green").setScale(scaling_parameter);
+		this.green4 = this.add.image(253*scaling_parameter, 80*scaling_parameter, "green").setScale(scaling_parameter);
 
-		this.yellow1 = this.add.image(218, 218, "yellow");
-		this.yellow2 = this.add.image(218, 253, "yellow");
-		this.yellow3 = this.add.image(253, 218, "yellow");
-		this.yellow4 = this.add.image(253, 253 , "yellow");
+		this.yellow1 = this.add.image(218*scaling_parameter, 218*scaling_parameter, "yellow").setScale(scaling_parameter);
+		this.yellow2 = this.add.image(218*scaling_parameter, 253*scaling_parameter, "yellow").setScale(scaling_parameter);
+		this.yellow3 = this.add.image(253*scaling_parameter, 218*scaling_parameter, "yellow").setScale(scaling_parameter);
+		this.yellow4 = this.add.image(253*scaling_parameter, 253*scaling_parameter , "yellow").setScale(scaling_parameter);
 	}
 
 	setinitData() {
@@ -133,41 +139,200 @@ class Scene2 extends Phaser.Scene {
 
 	move(pointer, gameObject){
 		
-		console.log(gameObject.getData("color"), " ", gameObject.getData("pos"));
+		console.log(gameObject.getData("color"), " ", gameObject.getData("pos"), " ", this.dice_roll);
 		if(1){			// Check if can move
 			
-			// Square width: 19.1
-			// First square: (16.8, 16.8)
-
+			var pos = gameObject.getData("pos");
 			// First Move
-			if(gameObject.getData("pos") == 0){
+			if(pos == 0){
 				switch(gameObject.getData("color")){
-					case "red":
-						gameObject.x = 35.9;
-						gameObject.y = 131.4;
-						break;
-					case "blue":
-						gameObject.x = 131.4;
-						gameObject.y = 265.1;
-						break;
-					case "green":
-						gameObject.x = 169.6;
-						gameObject.y = 35.9;
-						break;
-					case "yellow":
-						gameObject.x = 265.1;
-						gameObject.y = 169.6;
-						break;
+					case "red": pos = 1; break;
+					case "blue": pos = 40; break;
+					case "green": pos = 14; break;
+					case "yellow": pos = 27; break;
 				}
-				gameObject.setData("pos", gameObject.getData("pos") + 1);
-				// continue;
+			}
+			else if(gameObject.getData("color") == "red" && pos + this.dice_roll > 51){
+				if(pos <= 51){
+					pos += this.dice_roll + 1;
+				}
+				else if(pos + this.dice_roll < 59){
+					pos += this.dice_roll;
+				}
+				if(pos == 58){
+					this.redScore += 1;
+					if(this.redScore == 4){
+						this.scene.start("gameOver", {
+							redScore: this.redScore, 
+							blueScore: this.blueScore, 
+							greenScore: this.greenScore, 
+							yellowScore: this.yellowScore
+						});
+						console.log("RED WINS!!!!!!!!!!!!!!");
+					}
+					console.log("Red scored");
+					gameObject.destroy();
+				}
+			}
+			else if(gameObject.getData("color") == "blue" && (pos <= 38 || pos >= 68) && pos + this.dice_roll > 38){
+				if(pos <= 38){
+					pos += this.dice_roll + 29;
+				}
+				else if(pos + this.dice_roll < 74){
+					pos += this.dice_roll;
+				}
+				if(pos == 73){
+					this.blueScore += 1;
+					if(this.blueScore == 4){
+						this.scene.start("gameOver", {
+							redScore: this.redScore, 
+							blueScore: this.blueScore, 
+							greenScore: this.greenScore, 
+							yellowScore: this.yellowScore
+						});
+						console.log("BLUE WINS!!!!!!!!!!!!!!");
+					}
+					console.log("Blue scored");
+					gameObject.destroy();
+				}
+			}
+			else if(gameObject.getData("color") == "green" && (pos <= 12 || pos >= 58) && pos + this.dice_roll > 12){
+				if(pos <= 12){
+					pos += this.dice_roll + 45;
+				}
+				else if(pos + this.dice_roll < 64){
+					pos += this.dice_roll;
+				}
+				if(pos == 63){
+					this.greenScore += 1;
+					if(this.greenScore == 4){
+						this.scene.start("gameOver", {
+							redScore: this.redScore, 
+							blueScore: this.blueScore, 
+							greenScore: this.greenScore, 
+							yellowScore: this.yellowScore
+						});
+						console.log("GREEN WINS!!!!!!!!!!!!!!");
+					}
+					console.log("Green scored");
+					gameObject.destroy();
+				}
+			}
+			else if(gameObject.getData("color") == "yellow" && (pos <=25 || pos >= 63) && pos + this.dice_roll > 25){
+				if(pos <= 25){
+					pos += this.dice_roll + 37;
+				}
+				else if(pos + this.dice_roll < 69){
+					pos += this.dice_roll;
+				}
+				if(pos == 68){
+					this.yellowScore += 1;
+					if(this.yellowScore == 4){
+						this.scene.start("gameOver", {
+							redScore: this.redScore, 
+							blueScore: this.blueScore, 
+							greenScore: this.greenScore, 
+							yellowScore: this.yellowScore
+						});
+						console.log("YELLOW WINS!!!!!!!!!!!!!!");
+					}
+					console.log("Yellow scored");
+					gameObject.destroy();
+				}
+			}
+			else{
+				pos += this.dice_roll;
+				if(pos > 52){
+					pos -= 52;
+				}
 			}
 
-			// Stretch 1
-			// if()
-
-
+			// if pos is 73, dont show? gives console error o.w. Nothing significant
+			gameObject.setData("pos", pos);
+			[gameObject.x, gameObject.y] = this.get_new_position(pos);
+			[gameObject.x, gameObject.y] = [scaling_parameter * gameObject.x, scaling_parameter * gameObject.y]
 		}
+	}
+
+	get_new_position(location){
+		// Square width: 19.1
+		// First square: (16.8, 16.8)
+		
+		var mapping = new Map();
+		mapping.set(1, [35.9, 131.4]);
+		mapping.set(2, [55.0, 131.4]);
+		mapping.set(3, [74.1, 131.4]);
+		mapping.set(4, [93.2, 131.4]);
+		mapping.set(5, [112.3, 131.4]);
+		mapping.set(6, [131.4, 112.3]);
+		mapping.set(7, [131.4, 93.2]);
+		mapping.set(8, [131.4, 74.1]);
+		mapping.set(9, [131.4, 55.0]);
+		mapping.set(10, [131.4, 35.9]);
+		mapping.set(11, [131.4, 16.8]);
+		mapping.set(12, [150.5, 16.8]);
+		mapping.set(13, [169.6, 16.8]);
+		mapping.set(14, [169.6, 35.9]);
+		mapping.set(15, [169.6, 55.0]);
+		mapping.set(16, [169.6, 74.1]);
+		mapping.set(17, [169.6, 93.2]);
+		mapping.set(18, [169.6, 112.3]);
+		mapping.set(19, [188.7, 131.4]);
+		mapping.set(20, [207.8, 131.4]);
+		mapping.set(21, [226.9, 131.4]);
+		mapping.set(22, [246.0, 131.4]);
+		mapping.set(23, [265.1, 131.4]);
+		mapping.set(24, [285.2, 131.4]);
+		mapping.set(25, [285.2, 150.5]);
+		mapping.set(26, [285.2, 169.6]);
+		mapping.set(27, [265.1, 169.6]);
+		mapping.set(28, [246.0, 169.6]);
+		mapping.set(29, [226.9, 169.6]);
+		mapping.set(30, [207.8, 169.6]);
+		mapping.set(31, [188.7, 169.6]);
+		mapping.set(32, [169.6, 188.7]);
+		mapping.set(33, [169.6, 207.8]);
+		mapping.set(34, [169.6, 226.9]);
+		mapping.set(35, [169.6, 246.0]);
+		mapping.set(36, [169.6, 265.1]);
+		mapping.set(37, [169.6, 285.2]);
+		mapping.set(38, [150.5, 285.2]);
+		mapping.set(39, [131.4, 285.2]);
+		mapping.set(40, [131.4, 265.1]);
+		mapping.set(41, [131.4, 246.0]);
+		mapping.set(42, [131.4, 226.9]);
+		mapping.set(43, [131.4, 207.8]);
+		mapping.set(44, [131.4, 188.7]);
+		mapping.set(45, [112.3, 169.6]);
+		mapping.set(46, [93.2, 169.6]);
+		mapping.set(47, [74.1, 169.6]);
+		mapping.set(48, [55.0, 169.6]);
+		mapping.set(49, [35.9, 169.6]);
+		mapping.set(50, [16.8, 169.6]);
+		mapping.set(51, [16.8, 150.5]);
+		mapping.set(52, [16.8, 131.4]);
+		mapping.set(53, [35.9, 150.5]);
+		mapping.set(54, [55.0, 150.5]);
+		mapping.set(55, [74.1, 150.5]);
+		mapping.set(56, [93.2, 150.5]);
+		mapping.set(57, [112.3, 150.5]);
+		mapping.set(58, [150.5, 35.9]);
+		mapping.set(59, [150.5, 55.0]);
+		mapping.set(60, [150.5, 74.1]);
+		mapping.set(61, [150.5, 93.2]);
+		mapping.set(62, [150.5, 112.3]);
+		mapping.set(63, [265.1, 150.5]);
+		mapping.set(64, [246.0, 150.5]);
+		mapping.set(65, [226.9, 150.5]);
+		mapping.set(66, [207.8, 150.5]);
+		mapping.set(67, [188.7, 150.5]);
+		mapping.set(68, [150.5, 265.1]);
+		mapping.set(69, [150.5, 246.0]);
+		mapping.set(70, [150.5, 226.9]);
+		mapping.set(71, [150.5, 207.8]);
+		mapping.set(72, [150.5, 188.7]);
+
+		return mapping.get(location);
 	}
 
 
--- game.js
+++ game.js
@@ -1,10 +1,11 @@
-scaling_parameter = Math.min(Math.min(screen.width/420, screen.height/300), 1);
+// scaling_parameter = Math.min(Math.min(window.innerWidth/420, window.innerHeight/300), 1);
+scaling_parameter = 1;
 
 var config = {
-	width: 1 + scaling_parameter*420,
-	height: 1 + scaling_parameter*300,
+	width: scaling_parameter*421,
+	height: scaling_parameter*301,
 	backgroundColor: 0x000000,
-	scene: [Scene1, Scene2]
+	scene: [Scene1, Scene2, Scene3]
 }
 
 window.onload = function () {
